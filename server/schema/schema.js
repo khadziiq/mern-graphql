a7b893ac946d4c3b7a44eb3c6360151cd56069c9
khoirulkahf <fahrulista@gmail.com>
2022-08-25 14:33:21 +0700

    done project

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -96,6 +96,12 @@ const mutation = new GraphQLObjectType({
       type: ClientType,
       args: { id: { type: GraphQLNonNull(GraphQLID) } },
       resolve(parent, args) {
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.remove();
+          });
+        });
+
         return Client.findByIdAndRemove(args.id);
       },
     },
